468cc8a6c80944dfb26c4f9275aa19bcd91d6ab4
Yuri Simonov <yura15-96@mail.ru>
2021-04-18 14:02:52 +0300

    added store

--- src/components/Redux/state.js
+++ src/components/Redux/state.js
@@ -1,83 +1,90 @@
-import { rerenderEntireTree } from '../../render';
+let store = {
+	_state: {
+		myPage: {
+			MyPageData: [
+				{ id: 1, message: "Сегодня уже третий день, как я пытаюсь их понять... убейте меня, чтобы я не мучался...", },
+				{ id: 2, message: "Долбанные прототипы... Кто может объяснить их мне?", },
+				{ id: 3, message: "Сегодня начал изучать основы JavaScript", },
+				{ id: 4, message: "Что-то тут совсем тихо", },
+				{ id: 5, message: "Всем привет, я тут новенький!", },
 
-let state = {
-	myPage: {
-		MyPageData: [
-			{ id: 1, message: "Сегодня уже третий день, как я пытаюсь их понять... убейте меня, чтобы я не мучался...", },
-			{ id: 2, message: "Долбанные прототипы... Кто может объяснить их мне?", },
-			{ id: 3, message: "Сегодня начал изучать основы JavaScript", },
-			{ id: 4, message: "Что-то тут совсем тихо", },
-			{ id: 5, message: "Всем привет, я тут новенький!", },
+			],
+			/* friendsNames: [
+				{ id: 1, name: "Света", },
+				{ id: 2, name: "Рома", },
+				{ id: 3, name: "Игорь", },
+			], */
+			friendsNames: ["Света", "Рома", "Игорь"],
+			newPostText: 'новое текстовое значение поста на своей странице',
+		},
+		messagePage: {
+			smsData: [
+				{ id: 1, message: "Здарова", },
+				{ id: 2, message: "Как ты?", },
+				{ id: 3, message: "Ау", },
+				{ id: 4, message: "Ты тут?", },
+			],
+			newMessageText: 'новое текстовое значение поста в сообщениях',
+			messagesUser: [
+				{ id: 1, name: "Маша", },
+				{ id: 2, name: "Дима", },
+				{ id: 3, name: "Катя", },
+				{ id: 4, name: "Виктор", },
+				{ id: 5, name: "Максим", },
+				{ id: 6, name: "Григорий", },
+			],
+		},
 
-		],
-		/* friendsNames: [
-			{ id: 1, name: "Света", },
-			{ id: 2, name: "Рома", },
-			{ id: 3, name: "Игорь", },
-		], */
-		friendsNames: ["Света", "Рома", "Игорь"],
-		newPostText: 'новое текстовое значение поста на своей странице',
 	},
-	messagePage: {
-		smsData: [
-			{ id: 1, message: "Здарова", },
-			{ id: 2, message: "Как ты?", },
-			{ id: 3, message: "Ау", },
-			{ id: 4, message: "Ты тут?", },
-		],
-		newMessageText: 'новое текстовое значение поста в сообщениях',
-		messagesUser: [
-			{ id: 1, name: "Маша", },
-			{ id: 2, name: "Дима", },
-			{ id: 3, name: "Катя", },
-			{ id: 4, name: "Виктор", },
-			{ id: 5, name: "Максим", },
-			{ id: 6, name: "Григорий", },
-		],
+	getState() {
+		return this._state;
 	},
+	_callSubscriber() {
+		console.log("dfd");
+	},
+	//Добавление новых сообщений на стене "Моя страница"
+	addPost() {
+		let newPost = {
 
+			id: 6,
+			message: this._state.myPage.newPostText,
+		};
+
+		this._state.myPage.MyPageData.push(newPost);
+		this._state.myPage.newPostText = "";
+		this._callSubscriber(this._state);
+	},
+	//Добавление новых сообщений в переписках
+	addMessage() {
+
+		let newPost = {
+			id: 5,
+			message: this._state.messagePage.newMessageText,
+		};
+
+		this._state.messagePage.smsData.push(newPost);
+		this._state.messagePage.newMessageText = "";
+		this._callSubscriber(this._state);
+	},
+	//Обновление сообщений побуквенно при их вводе в textarea на стене "Моя страница"
+	updateNewPostText(newText) {
+
+		this._state.myPage.newPostText = newText;
+		this._callSubscriber(this._state);
+	},
+	//Обновление сообщений побуквенно при их вводе в textarea на стене "Cообщения"
+	updateNewMessageText(newMessage) {
+
+		this._state.messagePage.newMessageText = newMessage;
+		this._callSubscriber(this._state);
+	},
+	subscribe(observer) {
+		this._callSubscriber = observer;
+	},
 }
 
-//Добавление новых сообщений на стене "Моя страница"
-export let addPost = () => {
 
-	let newPost = {
-		id: 6,
-		message: state.myPage.newPostText,
-	};
+export default store;
 
-	state.myPage.MyPageData.push(newPost);
-	state.myPage.newPostText = "";
-	rerenderEntireTree(state);
-};
-
-//Добавление новых сообщений в переписках
-export let addMessage = () => {
-
-	let newPost = {
-		id: 5,
-		message: state.messagePage.newMessageText,
-	};
-
-	state.messagePage.smsData.push(newPost);
-	state.messagePage.newMessageText = "";
-	rerenderEntireTree(state);
-};
-
-//Обновление сообщений побуквенно при их вводе в textarea на стене "Моя страница"
-export let updateNewPostText = (newText) => {
-
-	state.myPage.newPostText = newText;
-	rerenderEntireTree(state);
-};
-
-//Обновление сообщений побуквенно при их вводе в textarea на стене "Cообщения"
-export let updateNewMessageText = (newMessage) => {
-
-	state.messagePage.newMessageText = newMessage;
-	rerenderEntireTree(state);
-};
-
-
-
-export default state;
+//Глобальное присваивание
+window.store = store;
